babb1e51dede917bb654eab32e63a424f4d53e3d
Daddypastor <emmaexcellent030@gmail.com>
2024-11-24 14:41:06 +0100

    Include authentication and saving note to database

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Transcrire | Your AI Note Taking",
